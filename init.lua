2a8c301ce5ba5445c4af8ef4cb98bc4be96d97fb
Isaac <42949629+IsaacWarren@users.noreply.github.com>
2024-04-22 15:18:29 -0400

    Remove vimrc, update lock, add octo

--- init.lua
+++ init.lua
@@ -1,6 +1,3 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 
-local vimrc = vim.fn.stdpath("config") .. "/vimrc.vim"
-vim.cmd.source(vimrc)
-
 require("config.lazy")
--- vimrc.vim
+++
@@ -1,5 +0,0 @@
-call plug#begin()
-
-Plug 'Pocco81/auto-save.nvim'
-call plug#end()
-
