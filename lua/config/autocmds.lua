755a828db5a2cc23a12e3fd6dd87724fb145210c
Isaac <42949629+IsaacWarren@users.noreply.github.com>
2024-09-11 11:15:54 -0500

    Fix and format

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -4,54 +4,78 @@
 --
 -- Disable autoformat
 vim.api.nvim_create_autocmd({ "FileType" }, {
-  pattern = { "**" },
-  callback = function()
-    vim.b.autoformat = false
-  end,
+    pattern = { "**" },
+    callback = function()
+        vim.b.autoformat = false
+    end,
 })
 
 -- Disable spellcheck in terminal
 vim.api.nvim_create_autocmd({ "TermOpen" }, {
-  pattern = { "*" },
+    pattern = { "*" },
+    callback = function()
+        vim.wo.spell = false
+    end,
+})
 
 --Fix background Transparency
-vim.api.nvim_create_autocmd({"SourcePost"}, {
-  pattern = {"*"},
-  callback = function()
-   local groups = { -- table: default groups
-    'Normal', 'NormalNC', 'Comment', 'Constant', 'Special', 'Identifier',
-    'Statement', 'PreProc', 'Type', 'Underlined', 'Todo', 'String', 'Function',
-    'Conditional', 'Repeat', 'Operator', 'Structure', 'LineNr', 'NonText',
-    'SignColumn', 'CursorLineNr', 'EndOfBuffer', 'NormalSB', 'Pmenu',
-  }
-  local extra_groups = { -- table: additional groups that should be cleared
-    'NormalFloat', -- plugins which have float panel such as Lazy, Mason, LspInfo
-    'FloatBorder',
-    'NvimTreeWinSeparator',
-    'NvimTreeNormal',
-    'NvimTreeNormalNC',
-    'TroubleNormal',
-    'TelescopeNormal',
-    'TelescopeBorder',
-    'WhichKeyFloat',
+vim.api.nvim_create_autocmd({ "SourcePost" }, {
+    pattern = { "*" },
+    callback = function()
+        local groups = { -- table: default groups
+            "Normal",
+            "NormalNC",
+            "Comment",
+            "Constant",
+            "Special",
+            "Identifier",
+            "Statement",
+            "PreProc",
+            "Type",
+            "Underlined",
+            "Todo",
+            "String",
+            "Function",
+            "Conditional",
+            "Repeat",
+            "Operator",
+            "Structure",
+            "LineNr",
+            "NonText",
+            "SignColumn",
+            "CursorLineNr",
+            "EndOfBuffer",
+            "NormalSB",
+            "Pmenu",
+        }
+        local extra_groups = { -- table: additional groups that should be cleared
+            "NormalFloat", -- plugins which have float panel such as Lazy, Mason, LspInfo
+            "FloatBorder",
+            "NvimTreeWinSeparator",
+            "NvimTreeNormal",
+            "NvimTreeNormalNC",
+            "TroubleNormal",
+            "TelescopeNormal",
+            "TelescopeBorder",
+            "WhichKeyFloat",
 
-    -- TODO: programmatically add this
-    'NotifyINFOBody',
-    'NotifyERRORBody',
-    'NotifyWARNBody',
-    'NotifyDEBUGBody',
-    'NotifyTRACEBody',
-    'NotifyINFOBorder',
-    'NotifyERRORBorder',
-    'NotifyWARNBorder',
-    'NotifyDEBUGBorder',
-    'NotifyTRACEBorder',
-  }
-  for _, group in ipairs(groups) do
-    vim.cmd(string.format("hi %s ctermbg=NONE guibg=NONE", group))
-  end
-  for _, group in ipairs(extra_groups) do
-    vim.cmd(string.format("hi %s ctermbg=NONE guibg=NONE", group))
-  end
-  end,
+            -- TODO: programmatically add this
+            "NotifyINFOBody",
+            "NotifyERRORBody",
+            "NotifyWARNBody",
+            "NotifyDEBUGBody",
+            "NotifyTRACEBody",
+            "NotifyINFOBorder",
+            "NotifyERRORBorder",
+            "NotifyWARNBorder",
+            "NotifyDEBUGBorder",
+            "NotifyTRACEBorder",
+        }
+        for _, group in ipairs(groups) do
+            vim.cmd(string.format("hi %s ctermbg=NONE guibg=NONE", group))
+        end
+        for _, group in ipairs(extra_groups) do
+            vim.cmd(string.format("hi %s ctermbg=NONE guibg=NONE", group))
+        end
+    end,
 })
