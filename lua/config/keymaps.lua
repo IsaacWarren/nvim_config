f48aaaea20ff68d70434a8eda8a3ca130d25baae
Isaac <42949629+IsaacWarren@users.noreply.github.com>
2024-11-11 13:46:39 -0600

    Set exit terminal keybind

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,7 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+--
+-- Exit terminal with CTRL-w
+vim.keymap.set("t", "<C-w>", "<C-\\><C-n>", {silent = true})
+
